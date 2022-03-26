395bee2ac008c0004a2778c4c332415d6fbc54c0
pro-soichiro <dr.1987626@gmail.com>
2022-03-27 08:19:35 +0900

    分岐と繰り返しの具体例

---
+++ fruits_list.rb
@@ -0,0 +1,20 @@
+fruits = ["りんご","いちご","パイナップル","ぶどう"]
+
+number = 0
+while number != 9
+  puts "フルーツ番号を入力してください（終了は9です）"
+  number = gets.to_i
+
+  if number == 1
+    puts fruits[0]
+  elsif number == 2
+    puts fruits[1]
+  elsif number == 3
+    puts fruits[2]
+  elsif number == 4
+    puts fruits[3]
+  else
+    puts "該当なし"
+  end
+
+end
